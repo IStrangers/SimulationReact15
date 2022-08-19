91ec20102ebea4abf5b18753d7ae4349f21f4f7b
qqq000qqq@aliyun.com <xqq1258460261>
2022-08-19 16:52:12 +0800

    20220819

--- packages/react/src/vdom.ts
+++ packages/react/src/vdom.ts
@@ -115,6 +115,7 @@ function updateElement(oldElement : any,newElement : any) {
   } else if(oldElement.nodeType === ELEMENT) {
     updateProps(currentDOM,oldElement.props,newElement.props)
     updateChildrenElement(currentDOM,oldElement.children,newElement.children)
+    oldElement.props = newElement.props
   } else if(oldElement.nodeType === CLASS_COMPONENT) {
     updateClassComponent(oldElement,newElement)
   } else if(oldElement.nodeType === FUNCTION_COMPONENT) {
@@ -131,8 +132,123 @@ function updateProps(dom : HTMLElement,oldProps : any,newProps : any) {
   setProps(dom,newProps)
 }
 
+let updateDepth = 0
+const diffQueue : Array<any> = []
 function updateChildrenElement(dom : HTMLElement,oldChildren : Array<any>,newChildren : Array<any>) {
-  
+  updateDepth++
+  diff(dom,oldChildren,newChildren)
+  updateDepth--
+  if(updateDepth === 0) {
+    patch()
+  }
+}
+
+function diff(dom : HTMLElement,oldChildren : Array<any>,newChildren : Array<any>) {
+  const oldChildrenElementsMap = getChildrenElementsMap(oldChildren)
+  const newChildrenElementsMap = getNewChildrenElementsMap(oldChildrenElementsMap,newChildren)
+  let lastIndex = 0
+  for(let i = 0; i < newChildren.length; i++) {
+    const newElement = newChildren[i]
+    if(!newElement) {
+      continue
+    }
+    const newKey = newElement.key || i.toString()
+    const oldElement = oldChildrenElementsMap[newKey]
+    if(oldElement === newElement) {
+      const mountIndex = oldElement.mountIndex
+      if(mountIndex < lastIndex) {
+        diffQueue.push({
+          parentNode: dom,
+          type: "MOVE",
+          fromIndex: mountIndex,
+          toIndex: i
+        })
+      }
+      lastIndex = Math.max(mountIndex,lastIndex)
+    } else {
+      diffQueue.push({
+        parentNode: dom,
+        type: "INSERT",
+        toIndex: i,
+        dom: createDOM(newElement)
+      })
+    }
+    newElement.mountIndex = i
+  }
+  for(let oldKey in oldChildrenElementsMap) {
+    if(newChildrenElementsMap.hasOwnProperty(oldKey)) {
+      const oldElement = oldChildrenElementsMap[oldKey]
+      diffQueue.push({
+        parentNode: dom,
+        type: "REMOVE",
+        fromIndex: oldElement.mountIndex
+      })
+    }
+  }
+}
+
+function patch() {
+  const deleteMap = {}
+  const deleteChildren = []
+  for(let i = 0; i < diffQueue.length; i++) {
+    const { type,fromIndex,parentNode } = diffQueue[i]
+    if(type === "MOVE" || type === "REMOVE") {
+      const oldElement = parentNode.children[fromIndex]
+      deleteMap[fromIndex] = oldElement
+      deleteChildren.push(oldElement)
+    }
+  }
+  deleteChildren.forEach(child => {
+    child.parentNode.removeChild(child)
+  })
+  for(let i = 0; i < diffQueue.length; i++) {
+    const { type,fromIndex,toIndex,parentNode,dom } = diffQueue[i]
+    if(type === "MOVE") {
+      insertChildAt(parentNode,deleteMap[fromIndex],toIndex)
+    } else if(type === "INSERT") {
+      insertChildAt(parentNode,dom,toIndex)
+    }
+  }
+  diffQueue.length = 0
+}
+
+function insertChildAt(parentNode : HTMLElement,child : Node,index : number) {
+  const oldChild = parentNode.children[index]
+  oldChild ? parentNode.insertBefore(child,oldChild) : parentNode.appendChild(child)
+}
+
+function getChildrenElementsMap(oldChildren : Array<any>) {
+  const oldChildrenElementsMap = {}
+  for(let i = 0; i < oldChildren.length; i++) {
+    const oldKey = oldChildren[i].key || i.toString()
+    oldChildrenElementsMap[oldKey] = oldChildren[i]
+  }
+  return oldChildrenElementsMap
+}
+
+function getNewChildrenElementsMap(oldChildrenElementsMap : any,newChildren : Array<any>) {
+  const newChildrenElementsMap = {}
+  for(let i = 0; i < newChildren.length; i++) {
+    const newElement = newChildren[i]
+    if(!newElement) {
+      continue
+    }
+    const newKey = newElement.key || i.toString()
+    const oldElement = oldChildrenElementsMap[newKey]
+    if(canDeepCompare(oldElement,newElement)) {
+      compareTwoElements(oldElement,newElement)
+      newChildren[i] = oldElement
+    }
+    newChildrenElementsMap[newKey] = newChildren[i]
+  }
+  return newChildrenElementsMap
+}
+
+function canDeepCompare(oldElement : any,newElement :any) {
+  if(!!oldElement && !!newElement) {
+    return oldElement.type === newElement.type
+  }
+  return false
 }
 
 function updateClassComponent(oldElement : any,newElement : any) {
@@ -158,6 +274,7 @@ export {
   compareTwoElements,
   updateElement,
   updateProps,
+  updateChildrenElement,
   updateClassComponent,
   updateFunctionComponent,
 }
